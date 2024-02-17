4cdd6c9af5a6508c08f2187ae1c90cd5dae36c5f
SureshLizzy <sureshforjira@gmail.com>
2024-02-17 15:55:18 +0530

    Added new method

--- src/test/java/Anime.java
+++ src/test/java/Anime.java
@@ -8,5 +8,17 @@ public class Anime {
 	private void pikachu() {
 		System.out.println("Ash's Pet");
 	}
+	
+	private void bandalero() {
+		System.out.println("The Hero of village peoples");
+	}
+	
+	private void poppeye() {
+		System.out.println("The power of spinach");
+	}
+	
+	private void sakalakaBoomBoom() {
+		System.out.println("The power of pencil");
+	}
 
 }
